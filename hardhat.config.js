790e7e76b9bf61cfa7635ddf3fa308c99c31948c
Shafly Hamzah <shafly2000@gmail.com>
2022-02-27 14:23:48 +0700

    Project init

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+const { ALCHEMY_API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.7",
+  defaultNetwork: "rinkeby", // Deploy network rinkeby testnet or ethereum mainnet
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: ALCHEMY_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    ethereum: {
+      chainId: 1,
+      url: ALCHEMY_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+  },
+  etherscan: {
+    apiKey: "EUGM9EVSMBNGJPA1QNZBQ4G3218DSN9RE5" // Test API
+  }
+}
