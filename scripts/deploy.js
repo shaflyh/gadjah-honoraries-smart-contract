ee6b949f5c90de0b52fe3deade1c8b40020b62f3
Shafly Hamzah <shafly2000@gmail.com>
2022-02-28 13:43:35 +0700

    Rename contract variable

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,26 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  // Get our account (as deployer) to verify that a minimum wallet balance is available
+  const [deployer] = await hre.ethers.getSigners();
+  console.log(`Deploying contracts with the account: ${deployer.address}`);
+  console.log(`Account balance: ${(await deployer.getBalance()).toString()}`);
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  // Fetch the compiled contract using ethers.js
+  const GadjahSocietyHonoraries = await hre.ethers.getContractFactory("GadjahSocietyHonoraries");
 
-  await greeter.deployed();
+  // Start deployment, calling deploy() will returning a promise that resolves to a contract object
+  const gadjahSociety = await GadjahSocietyHonoraries.deploy();
+  console.log("Contract deployed to address:", gadjahSociety.address);
+  const receipt = await gadjahSociety.deployTransaction.wait();
 
-  console.log("Greeter deployed to:", greeter.address);
+  // Smart contract gas fee deployment cost
+  console.log("Gas used: ", receipt.gasUsed._hex);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
+    console.error(error)
+    process.exit(1)
   });
